353c1a3fbf7fbcf2d867fb90c3d46cb02fe856ce
gonzaloandreozzi <andreozzigonzalo@gmail.com>
2022-10-17 19:05:59 +0200

    update app.js

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.get("/",(req, res) => {
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
 
-app.listen(process.env.PORT || 3001, () => console.log("Puerto 3000"));
+app.listen(process.env.PORT || 3001, () => console.log("Puerto 3001"));
 
