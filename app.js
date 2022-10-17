f085290750761b08361af625b763213269ee4fc9
gonzaloandreozzi <andreozzigonzalo@gmail.com>
2022-10-17 19:04:34 +0200

    actualizo mercado liebre

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.get("/",(req, res) => {
+    res.sendFile(path.join(__dirname, "views/index.html"))} 
+    );
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
+
+app.listen(process.env.PORT || 3001, () => console.log("Puerto 3000"));
+
